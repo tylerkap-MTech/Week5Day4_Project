c4e10cac8c1911e17c1fba5178ff1d79147d535b
tylerkap_MTech <tyler.kap3447@stu.mtec.edu>
2024-07-07 01:37:13 -0600

    First Committ

---
+++ app.js
@@ -0,0 +1,10 @@
+$(".navItem").css("color", "white");
+
+$("#nav").css("background-color", "#145");
+
+$("li:even").css("background-color", "#ccc");
+
+$("li:odd").css("background-color", "#aaa");
+
+$("input").prop("value", "Tyler Kap");
+
